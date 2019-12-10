91b79f39229ccba47d9c799041d4f8e8b64457ec
armstrom02 <viveksinghreborn@gmail.com>
2019-12-10 18:49:10 +0530

    created videoList

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,42 @@
 <template>
   <div>
-    <SearchBar ></SearchBar>
+    <SearchBar @termChange="onTermChange"></SearchBar>
+    <VideoList></VideoList>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 import SearchBar from "./components/SearchBar";
+import VideoList from "./components/VideoList";
+const API_KEY = "AIzaSyDMdvNTFrVfJaPZp1vAEKGZBWJvRaSM7C8";
 
 export default {
   name: "App",
   components: {
-    SearchBar
+    SearchBar,
+    VideoList
+  },
+  data() {
+    return {
+       
+    };
+  },
+  methods: {
+    onTermChange(searchTerm) {
+      axios
+        .get("https://www.googleapis.com/youtube/v3/search", {
+          params: {
+            key: API_KEY,
+            type: "video",
+            part: "snippet",
+            q: searchTerm
+          }
+        })
+        .then(res => {
+          console.log(res);
+        });
+    }
   }
 };
 </script>
---
+++ src/components/VideoList.vue
@@ -0,0 +1,14 @@
+<template> 
+<ul>
+    videoList
+</ul>
+</template>
+
+<script>
+export default {
+    name:'VideoList'
+};
+</script>
+
+<style>
+</style>
